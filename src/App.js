8f6f5726b927437f96857b489557672013cb7748
Pranav Cherkar <pranavcherkar123@gmail.com>
2023-09-24 18:41:06 +0530

    login page created

--- src/App.js
+++ src/App.js
@@ -3,17 +3,30 @@ import Cards from "./components/Cards";
 import { Routes, Route } from "react-router-dom";
 import AddMovie from "./components/AddMovie";
 import Detail from "./components/Detail";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import { createContext, useState, useEffect } from "react";
+const Appstate = createContext();
+
 function App() {
+  const [login, setLogin] = useState(false);
+  const [userName, setUserName] = useState("");
+
   return (
-    <div className="App relative">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Cards />} />
-        <Route path="/addmovie" element={<AddMovie />} />
-        <Route path="/detail/:id" element={<Detail />} />
-      </Routes>
-    </div>
+    <Appstate.Provider value={{ login, userName, setLogin, setUserName }}>
+      <div className="App relative">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Cards />} />
+          <Route path="/addmovie" element={<AddMovie />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </Appstate.Provider>
   );
 }
 
 export default App;
+export { Appstate };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import { Appstate } from "../App";
+
 const Header = () => {
+  const useAppState = useContext(Appstate);
   return (
     <div className="sticky z-10 bg-black text-3xl flex justify-between items-center font- bold p-3 border-b-2 border-gray-500">
       <Link to={"/"}>
@@ -11,14 +14,24 @@ const Header = () => {
           <span className="text-green-500">Movies</span>
         </span>
       </Link>
-      <Link to={"/addmovie"}>
-        <h1 className="text-lg flex items-center cursor-pointer text-white">
-          <Button>
-            <AddIcon className="mr-1" color="secondary" />
-            <span className="text-white">Add Movie</span>
-          </Button>
-        </h1>
-      </Link>
+      {useAppState.login ? (
+        <Link to={"/addmovie"}>
+          <h1 className="text-lg flex items-center cursor-pointer text-white">
+            <Button>
+              <AddIcon className="mr-1" color="secondary" />
+              <span className="text-white">Add Movie</span>
+            </Button>
+          </h1>
+        </Link>
+      ) : (
+        <Link to={"/login"}>
+          <h1 className="text-lg bg-green-500 flex items-center cursor-pointer text-white">
+            <Button>
+              <span className="text-white">Login</span>
+            </Button>
+          </h1>
+        </Link>
+      )}
     </div>
   );
 };
